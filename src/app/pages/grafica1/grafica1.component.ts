1d9c2857730cb76f58caa17ea1d450db9fa195d6
Irwin <irwinestradatorres@gmail.com>
2021-04-05 17:57:37 -0500

    Componente Incrementador y Dona

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
 
-  constructor() { }
+  public labels1: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public data1 = [
+    [50, 100, 150],
+  ];
 
-  ngOnInit(): void {
-  }
+  public labels2: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public data2 = [
+    [10, 450, 100],
+  ];
 
+  public labels3: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public data3 = [
+    [60, 120, 180],
+  ];
+
+  public labels4: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public data4 = [
+    [35, 45, 100],
+  ];
 }
