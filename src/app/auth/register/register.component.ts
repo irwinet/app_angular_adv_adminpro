bbf56aa5d64f535cfe8919b43df8c0b78078bdd8
irwinet <irwinestradatorres@gmail.com>
2021-09-12 19:01:36 -0500

    Valid Register Form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: [ './register.component.css' ]
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre: ['irwin', Validators.required],
+    email: ['irwinet@gmail.com', [Validators.required, Validators.email]],
+    password: ['12345', Validators.required],
+    password2: ['12345', Validators.required],
+    terminos: [true, Validators.required]
+  }, {
+    Validators: this.passwordsIguales('password', 'password2')
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+      console.log('posteando formulario!');
+    }
+    else {
+      console.log('formulario no es correcto!');
+    }
   }
 
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
+
+  contrasenasNoValidas() {
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+
+    if ((pass1 !== pass2) && this.formSubmitted) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  passwordsIguales(pass1Name: string, pass2Name: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if (pass1Control.value === pass2Control.value) {
+        pass2Control.setErrors(null);
+      }
+      else {
+        pass2Control.setErrors({ noEsIgual: true });
+      }
+    }
+  }
 }
