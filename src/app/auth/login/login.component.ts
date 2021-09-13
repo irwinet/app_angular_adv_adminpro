1d66d77d56f38653572259f97e4e9c5ea3166e0e
irwinet <irwinestradatorres@gmail.com>
2021-09-12 20:18:03 -0500

    Implement Login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +12,34 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public formSubmitted = false;
+
+  public loginForm = this.fb.group({
+    email: ['irwinet@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', Validators.required],
+    remember: [false]    
+  });
+
+  constructor(private router: Router,
+              private fb: FormBuilder,
+              private usuarioService : UsuarioService) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    this.router.navigateByUrl('/');
+    
+    this.usuarioService.login(this.loginForm.value)
+      .subscribe(resp  => {
+        console.log(resp);
+      }, (err) => {
+        // si sucede un error
+        Swal.fire('Error', err.error.msg, 'error');
+      });
+    
+    // console.log(this.loginForm.value);
+
+    // this.router.navigateByUrl('/');
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
+
+
+import { LoginForm } from '../interfaces/login-form.interface';
 import { RegisterForm } from '../interfaces/register-form.interface';
 
 const base_url = environment.base_url;
@@ -13,8 +17,24 @@ export class UsuarioService {
   constructor(private http: HttpClient) { }
 
   crearUsuario(formData: RegisterForm) {
-    
-    return this.http.post(`${base_url}/usuarios`, formData);
-    
+
+    return this.http.post(`${base_url}/usuarios`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
+
+  }
+
+  login(formData: LoginForm) {
+
+    return this.http.post(`${base_url}/login`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
+
   }
 }
