230e9ee1bcd3fc6377d8c4af6df66bfbf6a09b24
irwinet <irwinestradatorres@gmail.com>
2021-10-17 20:23:08 -0500

    Modiy Img User

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/usuario.model';
+import { FileUploadService } from 'src/app/services/file-upload.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -13,28 +14,52 @@ export class PerfilComponent implements OnInit {
 
   public perfilForm: FormGroup;
   public usuario: Usuario;
+  public imagenSubir: File;
+  public imgTemp: any = null;
 
-  constructor(private fb : FormBuilder,
-              private usuarioService: UsuarioService) {
-                this.usuario = usuarioService.usuario;
+  constructor(private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private fileUploadService: FileUploadService) {
+    this.usuario = usuarioService.usuario;
   }
 
   ngOnInit(): void {
     this.perfilForm = this.fb.group({
       nombre: [this.usuario.nombre, Validators.required],
       email: [this.usuario.email, [Validators.required, Validators.email]]
-    });      
+    });
   }
 
-  actualizarPerfil(){
+  actualizarPerfil() {
     // console.log(this.perfilForm.value);
     this.usuarioService.actualizarPerfil(this.perfilForm.value)
-        .subscribe(() => {
-          // console.log(resp);
-          const {nombre, email} = this.perfilForm.value;
-          this.usuario.nombre = nombre;
-          this.usuario.email = email;
-        });
+      .subscribe(() => {
+        // console.log(resp);
+        const { nombre, email } = this.perfilForm.value;
+        this.usuario.nombre = nombre;
+        this.usuario.email = email;
+      });
+  }
+
+  cambiarImagen(file: File) {
+    this.imagenSubir = file;
+
+    if (!file) {
+      return this.imgTemp = null;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = ()=>{
+      // console.log(reader.result);
+      this.imgTemp = reader.result;
+    };
+  }
+
+  subirImagen() {
+    this.fileUploadService.actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid)
+      .then(img => this.usuario.img = img);
   }
 
 }
