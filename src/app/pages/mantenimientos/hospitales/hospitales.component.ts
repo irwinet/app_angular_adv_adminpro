697aeab65b3da2199882ce65fac473a94ce0452c
irwinet <irwinestradatorres@gmail.com>
2021-10-24 22:21:04 -0500

    Get Hospitales

---
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from 'src/app/services/hospital.service';
+
+@Component({
+  selector: 'app-hospitales',
+  templateUrl: './hospitales.component.html',
+  styles: [
+  ]
+})
+export class HospitalesComponent implements OnInit {
+
+  public hospitales: Hospital[] = [];
+  public cargando: boolean = true;
+
+  constructor(private hospitalService: HospitalService) { }
+
+  ngOnInit(): void {
+    this.cargarHospitales();
+  }
+
+  cargarHospitales(){
+    this.cargando = true;
+
+    this.hospitalService.cargarHospitales()
+      .subscribe(hospitales => {
+        this.cargando = false;
+        this.hospitales = hospitales;
+      });
+  }
+
+}
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -12,6 +12,8 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
 
 const routes: Routes = [
     { 
@@ -28,7 +30,9 @@ const routes: Routes = [
           { path: 'perfil', component: PerfilComponent, data:{ titulo:'Perfil de Usuario' } },
 
           //Mantenimientos
-          { path: 'usuarios', component: UsuariosComponent, data:{ titulo:'Usuario de Aplicación' } },
+          { path: 'usuarios', component: UsuariosComponent, data:{ titulo:'Mantenimiento de Usuarios' } },
+          { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de Hospitales' } },
+          { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de Medicos' } }
         ]
     },
 
